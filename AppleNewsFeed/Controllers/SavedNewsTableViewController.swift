e399b12784ffd02aa79b82b95d40680974f7a963
artazidele <arta.zidele@gmail.com>
2021-02-16 13:15:00 +0200

    homework part two

--- AppleNewsFeed/Controllers/SavedNewsTableViewController.swift
+++ AppleNewsFeed/Controllers/SavedNewsTableViewController.swift
@@ -109,29 +109,30 @@ class SavedNewsTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
     }
-    */
+    
 
-    /*
+    
     // Override to support conditional rearranging of the table view.
     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
         return true
     }
-    */
+    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        guard let vc = storyboard.instantiateViewController(identifier: "WebStoryboard") as? WebViewController else { return }
+        
+        vc.urlString = savedItems[indexPath.row].url ?? ""
+        
+            
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
-    */
 
 }
